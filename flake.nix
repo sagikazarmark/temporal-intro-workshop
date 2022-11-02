91b357a963bf9a4d11949374aefb1e16e1ea4637
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-11-02 14:13:25 +0100

    chore: update nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,32 +1,24 @@
 {
-  description = "My workshop template";
+  description = "Temporal intro workshop";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    reveal-md-flake = {
-      url = "github:sagikazarmark/reveal-md-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, reveal-md-flake, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ reveal-md-flake.overlay ];
-        };
-      in {
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ git gnumake reveal-md go ];
+          buildInputs = with pkgs; [
+            git
+            gnumake
+            reveal-md
+            go
+          ];
         };
       });
 }
